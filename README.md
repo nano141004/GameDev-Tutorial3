79c10fa645a1e7cde0ac21d6450bb1e06475038a
Mariano Gerardus Senduk <121662118+nano141004@users.noreply.github.com>
2025-02-28 01:29:21 +0700

    Almost finish, added crouch animation. - Need readjust the collision shape when crouching, need to explain the works in readme, need to clean the code.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Referensi:
-- https://youtu.be/NhHpDcpY8ok?si=XqqYQWdhuYnvOlCs
-- https://godotforums.org/d/35106-is-there-a-simple-solution-for-a-double-tap
+- https://youtu.be/NhHpDcpY8ok?si=XqqYQWdhuYnvOlCs  : tutorial for making double jump and dashing mechanic
+- https://godotforums.org/d/35106-is-there-a-simple-solution-for-a-double-tap  : simple way to detect if a double tap occured, helps in making dashing mechanic
+- https://www.youtube.com/watch?v=sVJEaYNOUNw&list=PLhXFaKLHQJdXpwaNt6gGwpHLTWL0m-TSL&index=2&ab_channel=KaanAlpar  : how to make animations, facing left/right based on input
+- https://www.youtube.com/watch?v=Hpbn-w7H2V4&ab_channel=KaanAlpar  : tutorial for making crouching mechanic (animations too)
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var dash_duration = 0.25
 @export var gravity = 400.0
 @export var jump_speed = -300
+@export var cruch_walk_speed = 150
 var can_double_jump = false
 
 const DOUBLETAP_DELAY = 0.2
@@ -14,8 +15,15 @@ var last_direction = ""
 var is_dashing = false
 var dash_timer = 0.0
 
+var is_crouching = false
+
+#@onready var ap = AnimationPlayer
+#@onready var sprite = Sprite2D
+
 func _physics_process(delta):
 	velocity.y += delta * gravity
+	
+	#var direction = Input.get_axis("ui_left", "ui_right")
 
 	# timer dash
 	if is_dashing:
@@ -39,15 +47,42 @@ func _physics_process(delta):
 	if not is_on_floor() and Input.is_action_pressed("ui_down"):
 		velocity.y = walk_speed
 		
-	
+	# Crouch 
+	#if is_on_floor():
+		#if Input.is_action_pressed("ui_down"):
+			#print("tes")
+			#$AnimationPlayer.play("crouch_idle")
+		#if Input.is_action_just_released("ui_down"):
+			#$AnimationPlayer.play("crouch_walk")
+					
+		
 	# moving left and right
 	if not is_dashing:
 		if Input.is_action_pressed("ui_left"):
-			velocity.x = -walk_speed
+			
+			$Sprite2D.flip_h = true
+			if is_on_floor() and Input.is_action_pressed("ui_down"):
+				velocity.x = -cruch_walk_speed
+				$AnimationPlayer.play("crouch_walk")
+			else:
+				velocity.x = -walk_speed
+				$AnimationPlayer.play("walk")
 		elif Input.is_action_pressed("ui_right"):
-			velocity.x = walk_speed
+			
+			$Sprite2D.flip_h = false
+			if is_on_floor() and Input.is_action_pressed("ui_down"):
+				velocity.x = cruch_walk_speed
+				$AnimationPlayer.play("crouch_walk")
+			else:
+				velocity.x = walk_speed
+				$AnimationPlayer.play("walk")
 		else:
 			velocity.x = 0
+			if is_on_floor() and Input.is_action_pressed("ui_down"):
+				print("tes")
+				$AnimationPlayer.play("crouch_idle")
+			else:
+				$AnimationPlayer.play("idle")
 
 	move_and_slide()
 
