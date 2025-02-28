3537af7271d768c8914a88e08eb7908d17218dd1
Mariano Gerardus Senduk <121662118+nano141004@users.noreply.github.com>
2025-02-28 16:47:48 +0700

    finished the tutorial3

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 - https://godotforums.org/d/35106-is-there-a-simple-solution-for-a-double-tap  : simple way to detect if a double tap occured, helps in making dashing mechanic
 - https://www.youtube.com/watch?v=sVJEaYNOUNw&list=PLhXFaKLHQJdXpwaNt6gGwpHLTWL0m-TSL&index=2&ab_channel=KaanAlpar  : how to make animations, facing left/right based on input
 - https://www.youtube.com/watch?v=Hpbn-w7H2V4&ab_channel=KaanAlpar  : tutorial for making crouching mechanic (animations too)
+
+## Penjelesan pengerjaan implementasi fitur:
+- **Fitur double jump: **
+  
+    Terdapat var `can_double_jump`, yang mulanya bernilai false, dan jika player menekan arrow up pertama kali dari landasan untuk melakukan jump, var `can_double_jump` akan menjadi true. Player dapat melakukan jump lagi jika menekan arrow up, dan setelah itu var `can_double_jump` akan menjadi false kembali(mencegah bisa lebih dari 2 kali jump).
+
+- **Fitur dash:**
+    
+    Untuk melakukan dash, player harus terdeteksi menekan tombol arrow kiri 2 kali secara cepat, ataupun juga arrow kanan 2 kali secara cepat. Kemudian fitur dash akan aktif, dimana karakter player akan bergerak secara cepat ke kiri/kanan dalam beberapa waktu. Ide implementasi dengan membuat fungsi yang mendeteksi jika sebuah tombol arrow kiri/kanan ditekan 2 kali secara cepat, dan kemudian mengaktifkan mekanik dashing.
+
+- **Fitur moving down:**
+  
+    Jika karakter player berada di udara, maka player dapat menekan arrow down untuk membuat karakter player turun lebih cepat - layaknya movement ke kiri/kanan biasa.
+
+- **Fitur crouch:**
+
+    Jika karakter player berada di tanah/landasan, dan player menekan tombol arrow down, maka karakter akan crouch (karakter menjadi lebih pendek). Implementasi dilakukan dengan membuat animasi dengan node AnimationPlayer, ada animasi idle/berdiri biasa dan ada juga animasi crouch. Kemudian terdapat implementasi kode untuk mengganti animasi yang sesuai.
+
+- **Tambahan animasi:**
+    
+    Selain membuat animasi crouch, dibuat juga animasi idle, saat idle karakter terdapat animasi(sedikit pergerakan semacam bernafas, tidak hanya gambar static). Selain itu ada juga animasi disaat player menekan arrow kanan/bergerak ke kanan, karakter akan menghadap ke kanan, dan begitu juga sebaliknya untuk menghadap kiri. Ada juga animasi saat player berjalan, karakter player akan menjalankan animasi walk, dimana ada pergerakan karakter player seperti orang berjalan/berlari. Begitu juga saat crouch sambil bergerak ke kanan/kiri, ada juga animasi khusus.
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 @export var gravity = 400.0
 @export var jump_speed = -300
 @export var cruch_walk_speed = 150
+
 var can_double_jump = false
 
 const DOUBLETAP_DELAY = 0.2
@@ -17,20 +18,15 @@ var dash_timer = 0.0
 
 var is_crouching = false
 
-#@onready var ap = AnimationPlayer
-#@onready var sprite = Sprite2D
-
 func _physics_process(delta):
 	velocity.y += delta * gravity
 	
-	#var direction = Input.get_axis("ui_left", "ui_right")
-
 	# timer dash
 	if is_dashing:
 		dash_timer -= delta
 		if dash_timer <= 0:
 			is_dashing = false  # Stop dash
-			velocity.x = 0  
+			velocity.x = 0
 
 	# Double Jump Mechanic
 	# Second Jump
@@ -41,50 +37,42 @@ func _physics_process(delta):
 	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		velocity.y = jump_speed
 		can_double_jump = true
-		
+
 	# Moving down mechanic
 	# moving down faster if down arrow is being pressed while on air
 	if not is_on_floor() and Input.is_action_pressed("ui_down"):
 		velocity.y = walk_speed
-		
-	# Crouch 
-	#if is_on_floor():
-		#if Input.is_action_pressed("ui_down"):
-			#print("tes")
-			#$AnimationPlayer.play("crouch_idle")
-		#if Input.is_action_just_released("ui_down"):
-			#$AnimationPlayer.play("crouch_walk")
-					
-		
+
 	# moving left and right
 	if not is_dashing:
 		if Input.is_action_pressed("ui_left"):
-			
 			$Sprite2D.flip_h = true
-			if is_on_floor() and Input.is_action_pressed("ui_down"):
+			if is_on_floor() and Input.is_action_pressed("ui_down"): #crouching
 				velocity.x = -cruch_walk_speed
 				$AnimationPlayer.play("crouch_walk")
 			else:
 				velocity.x = -walk_speed
 				$AnimationPlayer.play("walk")
+				
 		elif Input.is_action_pressed("ui_right"):
-			
 			$Sprite2D.flip_h = false
-			if is_on_floor() and Input.is_action_pressed("ui_down"):
+			if is_on_floor() and Input.is_action_pressed("ui_down"): #crouching
 				velocity.x = cruch_walk_speed
 				$AnimationPlayer.play("crouch_walk")
-			else:
+			else: 
 				velocity.x = walk_speed
 				$AnimationPlayer.play("walk")
+				
+		#idle
 		else:
 			velocity.x = 0
-			if is_on_floor() and Input.is_action_pressed("ui_down"):
-				print("tes")
+			if is_on_floor() and Input.is_action_pressed("ui_down"): #crouching
 				$AnimationPlayer.play("crouch_idle")
-			else:
+			else: #standing
 				$AnimationPlayer.play("idle")
 
 	move_and_slide()
+
 
 func _input(event):
 	if event is InputEventKey and event.is_pressed() and not event.is_echo():
@@ -94,9 +82,9 @@ func _input(event):
 			direction = "left"
 		elif event.keycode == KEY_RIGHT:
 			direction = "right"
-		
+
 		if direction != "":
-			var now = Time.get_ticks_msec() / 1000.0  
+			var now = Time.get_ticks_msec() / 1000.0
 			if direction == last_direction and (now - last_tap_time) < DOUBLETAP_DELAY:
 				is_dashing = true
 				dash_timer = dash_duration
